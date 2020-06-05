8068897c6742375ee5bc5f8d33e8185d8c075b30
qwersety <63055743+qwersety@users.noreply.github.com>
2020-06-05 15:27:24 +0500

    Add files via upload
    комменты для компосера

--- js/composer.js
+++ js/composer.js
@@ -1,4 +1,4 @@
-// формирование блоков
+// формирование блоков (тег, данные по id  и классу, и контент)
 function composer(tag, id, clas, content) {
   let renderComposerString='';
   let identif='';
@@ -27,16 +27,21 @@ function renderGameField(name, myBG, enemyBG) {
   renderGameField=composer('div', 'gameFieldID', 'gameField', renderGameField);
   return renderGameField;
 }
-
+//генерируем структуру дебаг монитора
 function renderDebagPanel() {
   let debagString='';
+  // заголовок
   debagString+=composer('p', 'debagCap', 'debagCaptoin', 'Контроль действий:');
+  // вставка самой арены и первого сообщения
   debagString+=composer('textarea', 'debagArea', 'debagArea', 'Начало игры!');
+  // оборачиваем все в блок
   debagString=composer('div', '', '', debagString);
   return debagString;
 }
 
+// генерируем статистику вражеского флота
 function renderGameStat() {
+  // создаются строки длля названия корабля, дефисы и значения (стартовые, всегда одинаковы)
   let fourDeck=composer('p', '', 'statOptions', 'четырёхпалубныe');
   let threeDeck=composer('p', '', 'statOptions', 'трёхпалубныe');
   let twoDeck=composer('p', '', 'statOptions', 'двухпалубныe');
@@ -46,10 +51,13 @@ function renderGameStat() {
   let threeDeckStat=composer('p', 'deckStat-3', 'statOptions', '2');
   let twoDeckStat=composer('p', 'deckStat-2', 'statOptions', '3');
   let oneDeckStat=composer('p', 'deckStat-1', 'statOptions', '4');
+  // шапка
   let statText='Действующие вражеские корабли:'
   let stringGameStat='';
   stringGameStat+=composer('p', '', 'statOptionsText', statText);
+  // соединение в общую строку
   stringGameStat+=fourDeck+dash+fourDeckStat+threeDeck+dash+threeDeckStat+twoDeck+dash+twoDeckStat+oneDeck+dash+oneDeckStat;
+  // оборачиваем все в блок
   stringGameStat=composer('div', 'enemyState', 'enemyStat', stringGameStat);
   return stringGameStat;
 }
